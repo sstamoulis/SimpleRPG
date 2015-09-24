85b5324fee7daf7b85bdfd8d6591205a36915dad
Stathis <sstamoulis2010@gmail.com>
2015-09-24 17:04:06 +0300

    Changing SkillsList

--- SimpleRPG/SkillsList.cs
+++ SimpleRPG/SkillsList.cs
@@ -8,19 +8,12 @@ namespace SimpleRPG
 {
 	public class SkillsList
 	{
-		const int SLOTS_LIMIT = 4;
-		OrderedDictionary Skills = new OrderedDictionary(SLOTS_LIMIT);
+		int countLimit;
+		Dictionary<string,Skill> SkillsByName = new Dictionary<string, Skill>();
+		List<Skill> SkillsByIndex = new List<Skill>();
 
-		/// <summary>
-		/// Adds a skill to the SkillsList.
-		/// If there is no empty slot, it will throw an exception.
-		/// </summary>
-		/// <returns>The index of the skill</returns>
-		/// <param name="skill">The skill to add.</param>
-		/// <exception cref="System.InvalidOperationException">Thrown when there are no empty slots available.</exception>
 		public void Add(Skill skill) {
-			if (Skills.Count >= SLOTS_LIMIT) throw new InvalidOperationException("There is no empty skill slot!");
-			Skills.Add(skill.Name, skill);
+			SkillsByName.Add(skill.Name, skill);
 		}
 
 		public void Remove(int index) {
@@ -44,11 +37,7 @@ namespace SimpleRPG
 		public Skill this[int index] => GetSkill(index);
 
 		public Skill GetSkill(string skillName) {
-			for (int i = 0; i < Skills.Length; i++) {
-				if (Skills[i].Name.Equals(skillName, StringComparison.OrdinalIgnoreCase))
-					return Skills[i];
-			}
-			return null;
+			return Skills[skillName];
 		}
 		public Skill this[string skillName] => GetSkill(skillName);
 	}
